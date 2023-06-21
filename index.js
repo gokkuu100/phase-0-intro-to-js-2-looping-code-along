917d701c6293c67eb9a21cf1b0bf4b376af8aa8a
goku <wkurts247@gmail.com>
2023-06-21 12:53:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(array, name) {
+    let newArrays =[];
+    for (let i= 0; i < array.length; i++) {
+      let emptyArray =`Thank you, ${array[i]}, for the wonderful ${name} gift!`;
+      newArrays.push(emptyArray);
+    }
+    return newArrays;
+  }
+  console.log(writeCards(["Hope","Prince"], "house"));
+
+
+
+function countDown(i) {
+    let a = 0;
+    while (a<=i) {
+        console.log(a);
+        a++;
+    }
+}
+
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log(countDown(10));
